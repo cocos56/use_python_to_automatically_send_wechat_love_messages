3c42e0ea751721d23d8803420b51c1f47fc1ac23
C1 <zj175@139.com>
2022-05-28 18:19:07 +0800

    调整架构

--- send_msg.py
+++ send_msg.py
@@ -15,8 +15,8 @@ def get_weather(area_code):
     """
     try:
         with urlopen(f'https://weather.mipang.com/tianqi-{area_code}') as html:
-            html_read = bytes.decode(html.read()).replace("\n", "").replace("\r", "")
-            return html_read.split("row row1")[1].split(">")[1].split("<")[0]
+            html_read = bytes.decode(html.read()).replace('\n', '').replace('\r', '')
+            return html_read.split('row row1')[1].split('>')[1].split('<')[0]
     except HTTPError:
         time.sleep(1)
         return get_weather(area_code)
@@ -26,25 +26,25 @@ def send_msg(name, msg):
     """
     发送信息
     :param name: 联系人名字（备注）
-    :param msg: 待发送的信息，支持元组（多条信息）、字符串和列表（单条信息）
+    :param msg: 待发送的信息，支持列表（多条信息）、字符串（单条信息单行）和元组（单条信息多行）
     """
     # 打开微信
     pyautogui.hotkey('ctrl', 'alt', 'w')
     time.sleep(1)
     # 查找联系人
-    pyautogui.hotkey("ctrl", "f")
+    pyautogui.hotkey('ctrl', 'f')
     pyperclip.copy(name)
     pyautogui.hotkey('ctrl', 'v')
     time.sleep(1)
     pyautogui.hotkey('Enter')
     time.sleep(1)
     # 粘贴并发送信息
-    if not isinstance(msg, tuple):
-        msg = tuple([msg])
+    if not isinstance(msg, list):
+        msg = [msg]
     for i in msg:
         if isinstance(i, str):
             pyperclip.copy(i)
-        elif isinstance(i, list):
+        elif isinstance(i, tuple):
             pyperclip.copy('\n'.join(i))
         pyautogui.hotkey('ctrl', 'v')
         pyautogui.hotkey('Enter')
