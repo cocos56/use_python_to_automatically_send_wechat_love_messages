8254ae02f0cf37152be92621a525a0cd5e62c1e2
C1 <zj175@139.com>
2022-05-28 18:23:17 +0800

    添加函数说明

--- base.py
+++ base.py
@@ -5,4 +5,9 @@ from random import randint
 
 
 def get_random_item(items):
+    """
+    从多个元素中随机获取其中的某一个元素
+    :param items: 多个元素
+    :return: 多个元素中的某一个元素
+    """
     return items[randint(0, len(items) - 1)]
