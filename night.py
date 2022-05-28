1aa0cb6d70b795a1b0cb4ca0d8b89bdf84c47a54
C1 <zj175@139.com>
2022-05-28 23:01:31 +0800

    修复无法正常发送晚安文案的问题

--- night.py
+++ night.py
@@ -8,4 +8,4 @@ from pet_name import get_pet_name
 
 if __name__ == '__main__':
     send_msg('18739776523', '晚安')
-    send_msg('慧宝', [get_night_msg, f'{get_pet_name()}晚安'])
+    send_msg('慧宝', [get_night_msg(), f'{get_pet_name()}晚安'])
